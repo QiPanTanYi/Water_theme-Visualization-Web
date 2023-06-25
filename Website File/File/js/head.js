8023796af7b98e59673891f68207edf7b9aa8d5a
谭谭 <100819988+QiPanTanYi@users.noreply.github.com>
2023-06-26 00:17:50 +0800

    Delete Website File directory

--- Website File/File/js/head.js
+++
@@ -1,7 +0,0 @@
-var headMax = document.querySelector('#headMax')
-var headBtn = document.querySelector('.bottle')
-headBtn.addEventListener('click',function(){
-    $('#headMax').css('display','none')
-    $('#one-them').css('display','block')
-    // headMax.remove()
-})
